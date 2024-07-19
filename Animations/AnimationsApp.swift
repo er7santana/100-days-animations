cf3be2bd584e51d73d10489c344f1ef1dc8964a6
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-19 08:37:55 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
